0e7cb5fb4c7f91f75dcfbfc817a19e67aee74dca
dbohdiev <dbohdiev@vmware.com>
2023-04-10 11:54:19 +0300

    Lab 4

--- apps/store/src/app/app.component.ts
+++ apps/store/src/app/app.component.ts
@@ -3,13 +3,14 @@ import { Component } from '@angular/core';
 import { getAllGames } from '../fake-api';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { StoreUiSharedModule } from "@bg-hoard/store/ui-shared";
 
 @Component({
   selector: 'bg-hoard-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [ MatCardModule, RouterModule, CommonModule ]
+  imports: [ MatCardModule, RouterModule, CommonModule, StoreUiSharedModule ]
 })
 export class AppComponent {
   title = 'Board Game Hoard';
